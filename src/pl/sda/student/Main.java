72c45b032085386816ef632a7e4753b893e396dd
Lukasz <lukasz.ogan@gmail.com>
2020-02-29 13:52:33 +0100

    jsonutils

--- src/pl/sda/student/Main.java
+++ src/pl/sda/student/Main.java
@@ -5,6 +5,8 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 
@@ -13,13 +15,14 @@ public class Main {
         Student student = new Student("Jan",
                 "Kowalski",
                 32);
+        List<Student> studentList = new ArrayList<>();
+        studentList.add(student);
+        studentList.add(student);
+        studentList.add(student);
+        JSONUtils jsonUtils = new JSONUtils();
+        jsonUtils.writeList("lista.json", studentList);
 
-        ObjectMapper objectMapper = new ObjectMapper();
+        jsonUtils.readList("lista.json");
 
-        try {
-            objectMapper.writeValue(new File("student.json"), student);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
     }
 }
